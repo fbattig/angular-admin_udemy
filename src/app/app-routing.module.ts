2bb9e1b02eaba6b73023255880bb96057516d8a1
Felix Battig <fbattig@yahoo.com>
2019-02-23 08:27:07 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule,  Routes  } from '@angular/router';
 
-const routes: Routes = [];
+import { PagesComponent } from './pages/pages.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '', component: PagesComponent, children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    
+  ] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+  { path: '**', component: NopagefoundComponent }
+  
+];
+
+export const APP_ROUTE = RouterModule.forRoot(appRoutes, { useHash: true });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule,  Routes  } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+// Routes
+import { APP_ROUTE } from './app-routing.module';
+  
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProgressComponent,  
+    Graficas1Component,
+    NopagefoundComponent,
+    PagesComponent 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTE    
   ],
   providers: [],
   bootstrap: [AppComponent]
