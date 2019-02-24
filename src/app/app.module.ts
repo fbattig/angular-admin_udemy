79ecf36803ebf56ad05a43e6661d860b94100773
Felix Battig <fbattig@yahoo.com>
2019-02-24 16:21:02 -0500

    Modules modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule,  Routes  } from '@angular/router';
 
 // Routes
 import { APP_ROUTE } from './app-routing.module';
-  
+
+//Modules
+import { PageModules } from './pages/page.modules';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
-    LoginComponent,
-    ProgressComponent,  
-    Graficas1Component,
-    NopagefoundComponent,
-    PagesComponent,
-    RegisterComponent 
-
-  ],
-  imports: [
-    BrowserModule,
-    APP_ROUTE    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent
+	],
+	imports: [BrowserModule, APP_ROUTE, PageModules],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
